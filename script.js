ee00c0331c8dc9639de8fc50643d374669e94643
Rhowen Raju <rhowenraju@Rhowens-MacBook-Pro.local>
2025-04-03 16:22:25 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Script for navigation bar
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('nav');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    });
+    if (bar) {
+        bar.addEventListener('click', () => {
+            nav.classList.add('active');
+        });
+    }
+}
